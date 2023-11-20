314331d0a93ebe45eaa51f50b31dacaef82eb2d1
Emad Shoaib <emadshoaib1403@gmail.com>
2023-11-21 05:05:19 +0530

    somewhat changes in main activity.java

---
+++ Docket/app/src/main/java/com/smartherd/docket/Adapter/ToDoAdapter.java
@@ -0,0 +1,61 @@
+package com.smartherd.docket.Adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.smartherd.docket.MainActivity;
+import com.smartherd.docket.Model.ToDoModel;
+import com.smartherd.docket.R;
+
+import java.util.List;
+
+public class ToDoAdapter extends RecyclerView.Adapter<ToDoAdapter.ViewHolder> {
+
+    private List<ToDoModel> todoList;
+    private MainActivity activity;
+    public ToDoAdapter(MainActivity activity){
+        this.activity = activity;
+
+    }
+
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType){
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.task_layout, parent, false);
+        return new ViewHolder(itemView);
+    }
+
+    public void onBindViewHolder(ViewHolder holder,int position){
+        ToDoModel item = todoList.get(position);
+        holder.task.setText(item.getTask());
+        holder.task.setChecked(toBoolean(item.getStatus()));
+    }
+
+    // to get count of items in list
+    public int getItemCount(){
+        return todoList.size();
+    }
+
+    // boolean function to convert int to boolean
+    private boolean toBoolean(int n){
+        return n!=0;
+    }
+
+    public void setTasks(List<ToDoModel> todoList){
+        this.todoList = todoList;
+        notifyDataSetChanged();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder{
+        CheckBox task;
+        ViewHolder(View view){
+            super(view);
+            task=view.findViewById(R.id.todoCheckBox);
+        }
+    }
+
+
+}
--- Docket/app/src/main/java/com/smartherd/docket/SplashActivity.java
+++ Docket/app/src/main/java/com/smartherd/docket/SplashActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+
+import java.util.Objects;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -11,12 +14,17 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        getSupportActionBar().hide();    // hides support action bar from activity
 
-        final Intent i = new Intent(SplashActivity.this,MainActivity.class);
-        
+        Objects.requireNonNull(getSupportActionBar()).hide();
 
-
+        final Intent i = new Intent(SplashActivity.this, MainActivity.class);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(i);
+                finish();
+            }
+        }, 2000);
 
     }
 }
